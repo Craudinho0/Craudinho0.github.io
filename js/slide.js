4ca9f084b34705f53a2d1e4a30f6427c865ea5ac
Craudinho0 <andre17almeidah@gmail.com>
2025-04-19 17:08:25 -0300

    Delete js directory

--- js/slide.js
+++
@@ -1,9 +0,0 @@
-$('.rslides').responsiveSlides({
-    auto: false,             
-    speed: 200,           
-    timeout: 500,       
-});
-
-$(function() {
-    $(".rslides").responsiveSlides();
-});
